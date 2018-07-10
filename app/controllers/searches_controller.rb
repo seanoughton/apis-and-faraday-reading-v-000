9df0d839ddd6d42d23cf9c814fa02ff498626750
Sean Oughton <seanoughton@gmail.com>
2018-07-10 17:21:58 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,7 +11,7 @@ class SearchesController < ApplicationController
       req.params['query'] = 'coffee shop'
     end
     byebug
-    body_hash = JSON.parse(@resp.body)
+    body = JSON.parse(@resp.body)
     if @resp.success?
       @venues = body["response"]["venues"]
     else
